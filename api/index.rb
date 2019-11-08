d7328429883ca697f67474926be6d5d26f53a5c8
userman123 <dominic.freeman123@gmail.com>
2019-11-08 12:56:53 +0000

    Remove cowsay

--- api/index.rb
+++ api/index.rb
@@ -3,5 +3,5 @@ require 'cowsay'
 Handler = Proc.new do |req, res|
   res.status = 200
   res['Content-Type'] = 'text/plain'
-  res.body = Cowsay.say('hello world', 'cow')
+  res.body = "Hey bud"
 end
