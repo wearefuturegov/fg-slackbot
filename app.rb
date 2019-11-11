1e8cbf5be91ec0ddeb7d232515fb6f548a5961bf
userman123 <dominic.freeman123@gmail.com>
2019-11-11 16:16:28 +0000

    Test posting to two channels

--- app.rb
+++ app.rb
@@ -30,7 +30,13 @@ post '/events' do
 
       client.chat_postMessage(
         as_user: 'false',
-        channel: '#general',
+        channel: '#hello-dom',
+        text: ":satellite_antenna: <!here> New channel created: <##{request_data['event']['channel']['id']}> by: <@#{request_data['event']['channel']['creator']}>\nDescription: \"_#{channel_purpose}_\""
+      )
+
+      client.chat_postMessage(
+        as_user: 'false',
+        channel: '#hello_world',
         text: ":satellite_antenna: <!here> New channel created: <##{request_data['event']['channel']['id']}> by: <@#{request_data['event']['channel']['creator']}>\nDescription: \"_#{channel_purpose}_\""
       )
 
